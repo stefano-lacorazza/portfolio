9b6ddf5cbc49406c6095361681fd4470a2632401
stefano-lacorazza <112132737+stefano-lacorazza@users.noreply.github.com>
2023-07-24 08:02:55 -0500

    update project apps

--- myportfolio/mainsite/urls.py
+++ myportfolio/mainsite/urls.py
@@ -1,11 +1,14 @@
 from django.contrib import admin
 from django.urls import include, path
+import project1
+import project2
+
 from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),
-    path("proyect1/", views.proyect1, name="proyect1"),
-    path("proyect2/", views.proyect2, name="proyect2"),
-    path("proyect3/", views.proyect3, name="proyect3"),
+    #path("project1/", include("project1.urls")),
+    path("project2/", include("project2.urls")),
+    #path("project3/", include("project3.urls")),
     path("admin/", admin.site.urls),
 ]
