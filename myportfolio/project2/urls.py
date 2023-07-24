f07d812d5863e78995ed69573442bc3ef6436743
stefano-lacorazza <112132737+stefano-lacorazza@users.noreply.github.com>
2023-07-24 11:44:26 -0500

    Create project 2 site

--- myportfolio/project2/urls.py
+++ myportfolio/project2/urls.py
@@ -1,8 +1,10 @@
 from django.contrib import admin
 from django.urls import include, path
+from . import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+     path("", views.project2, name="project2main"),
    
     
 ]
--- myportfolio/project2/views.py
+++ myportfolio/project2/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
 
 # Create your views here.
+def project2(request):
+    """_summary_
+
+    Args:
+        request (_type_): _description_
+
+    Returns:
+        _type_: _description_
+    """
+
+    context = {
+        "key": 0,
+    }
+    return render(request, 'Project2\proyect2.html', context)
