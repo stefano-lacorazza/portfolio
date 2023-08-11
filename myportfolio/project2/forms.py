d659a0aff2552356b701ec8958ed68711d42869e
stefano-lacorazza <112132737+stefano-lacorazza@users.noreply.github.com>
2023-08-11 11:02:41 -0500

    added address to lat lon

--- myportfolio/project2/forms.py
+++ myportfolio/project2/forms.py
@@ -7,7 +7,8 @@ city_Choices = (
 )   
 class rentForm(forms.Form):
     city = forms.ChoiceField(label="City", choices =city_Choices )
-    location = PlainLocationField(based_fields=['city'], zoom=7)
+    #location = PlainLocationField(based_fields=['city'], zoom=7)
+    address = forms.CharField(label='Property address')
     built_Area = forms.IntegerField(label='Buit area in m2')
     private_Area = forms.IntegerField(label='Total area in m2')
     stratum = forms.IntegerField(label = 'Estrato' )
@@ -15,5 +16,4 @@ class rentForm(forms.Form):
     parking_lots = forms.IntegerField(label = 'Number of parking lots' )
     bathrooms = forms.IntegerField(label = 'Number of bathrooms')
     age = forms.IntegerField(label = 'Age of the property in years')
-    lat = forms.DecimalField(label = 'Latitude')
-    lon = forms.DecimalField(label = 'Longitude')
+    
--- myportfolio/project2/views.py
+++ myportfolio/project2/views.py
@@ -4,6 +4,7 @@ import pickle
 import os
 import pandas as pd
 from django.http import HttpResponse
+import requests
 # Create your views here.
 def project2(request):
 
@@ -16,10 +17,20 @@ def project2(request):
         if form.is_valid():
             # process the data in form.cleaned_data as required
             model_path = os.path.join(os.path.dirname(__file__), 'rent_predictor_model.pkl')
+            address = form.cleaned_data.get('address').join(', ').join(form.cleaned_data.get('city'))
+            api_key = 'AIzaSyCuORg0sZFB_hEBVrNPieRnLgqyA8jrlIw'
+
+            api_response = requests.get(
+            'https://maps.googleapis.com/maps/api/geocode/json?address={0}&key={1}'.format(address, api_key))
+            api_response_dict = api_response.json()
+
+            if api_response_dict['status'] == 'OK':
+                latitude = api_response_dict['results'][0]['geometry']['location']['lat']
+                longitude = api_response_dict['results'][0]['geometry']['location']['lng']
 
             with open(model_path, 'rb') as model_file:
                 model_RFR = pickle.load(model_file)
-            apto = pd.DataFrame([[form.cleaned_data.get('city'),form.cleaned_data.get('built_Area'),form.cleaned_data.get('private_Area'),form.cleaned_data.get('stratum'),form.cleaned_data.get('rooms'),form.cleaned_data.get('parking_lots'),form.cleaned_data.get('bathrooms'),form.cleaned_data.get('age'),form.cleaned_data.get('lat'),form.cleaned_data.get('lon')]])
+            apto = pd.DataFrame([[form.cleaned_data.get('city'),form.cleaned_data.get('built_Area'),form.cleaned_data.get('private_Area'),form.cleaned_data.get('stratum'),form.cleaned_data.get('rooms'),form.cleaned_data.get('parking_lots'),form.cleaned_data.get('bathrooms'),form.cleaned_data.get('age'),latitude,longitude]])
             
             # redirect to a new URL:
             return render(request, 'Project2/proyect2.html', {"form": form , "prediction": '$' +str(model_RFR.predict(pd.DataFrame(apto))[0])+' COP'})
