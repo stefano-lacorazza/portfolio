316a37e0c49e9cbcbc922b18162da70987e74530
stefano-lacorazza <112132737+stefano-lacorazza@users.noreply.github.com>
2023-08-11 13:06:18 -0500

    Update views.py

--- myportfolio/project2/views.py
+++ myportfolio/project2/views.py
@@ -17,7 +17,7 @@ def project2(request):
         if form.is_valid():
             # process the data in form.cleaned_data as required
             model_path = os.path.join(os.path.dirname(__file__), 'rent_predictor_model.pkl')
-            address1 = form.cleaned_data.get('address')+'%20'+form.cleaned_data.get('city')
+            address1 = form.cleaned_data.get('address')+'%20'+num_to_city(form.cleaned_data.get('city'))
             address = address1.replace(' ','%20').replace('#','%23')
             api_key = 'AIzaSyB7ll_7odkimkj8vly0VIt-Dol9useuv5Q'
             
@@ -47,3 +47,33 @@ def project2(request):
     }
     return render(request, 'Project2/proyect2.html',  {'form': form})
 
+
+def num_to_city(p_num):
+    if p_num == '0':
+        return 'Bogotá'
+    elif p_num == '2':
+        return 'Barranquilla'
+    elif p_num == '4':
+        return 'Medellín'
+    elif p_num == '5':
+        return 'Cali'
+    elif p_num == '6':
+        return 'Santa Marta'
+    elif p_num == '7':
+        return 'Cartagena'
+    elif p_num == '8':
+        return 'Pereira'
+    elif p_num == '9':
+        return 'Bucaramanga'
+    elif p_num == '10':
+        return 'Montería'
+    elif p_num == '11':
+        return 'Envigado'
+    elif p_num == '12':
+        return 'Villavicencio'
+    else:
+        return 'Otro'
+
+
+    ('0', 'Bogotá'), ( '1',), ( '4',''), ('5',''), ('6',''), ('7',''), 
+ ('8',''), ( '9',''), ('10',''),('11',''),('12',''),( '99','Otro') 
