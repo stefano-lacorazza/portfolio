05dede9dce606471e007403896e2f44bb9ff5188
stefano-lacorazza <112132737+stefano-lacorazza@users.noreply.github.com>
2023-08-21 00:52:51 -0500

    Update predict.py

--- myportfolio/project4/predict.py
+++ myportfolio/project4/predict.py
@@ -20,33 +20,34 @@ def deleteQR():
         print("Error: %s : %s" % (dir_path, e.strerror))
 
 def predict(p_image):
-    model_path = os.path.join(os.path.dirname(__file__), 'best.pt')
+#    model_path = os.path.join(os.path.dirname(__file__), 'best.pt')
 
-    with open(model_path, 'rb') as model_file:
-        model = pickle.load(model_file)
-    deleteQR()
-    source = p_image
-    results = model.predict(source, conf =0.7)
-    num_results = len(results[0].boxes.data)
-    qcd = cv2.QRCodeDetector()
-    for r in results:
-            r.save_crop('')
-    time.sleep(3)
+ #   with open(model_path, 'rb') as model_file:
+#        model = pickle.load(model_file)
+#    deleteQR()
+#    source = p_image
+ #   results = model.predict(source, conf =0.7)
+ #   num_results = len(results[0].boxes.data)
+ #   qcd = cv2.QRCodeDetector()
+ #   for r in results:
+ #           r.save_crop('')
+ #   time.sleep(3)
 
     urls = ()
     imgs = ()
-    for i in range(num_results):
-        if i == 0:
-            source = 'QR-codes/im.jpg'
+  #  for i in range(num_results):
+  #      if i == 0:
+  #          source = 'QR-codes/im.jpg'
             
-        else:
-            j =str(i+1)
-            source = 'QR-codes/im'+j+'.jpg'
+  #      else:
+   #         j =str(i+1)
+   #         source = 'QR-codes/im'+j+'.jpg'
      
-        img = cv2.imread(source)
-        imgs.append(source)
-        retval, decoded_info, points, straight_qrcode = qcd.detectAndDecodeMulti(img)
-        urls.append(decoded_info)
+   #     img = cv2.imread(source)
+   #     imgs.append(source)
+  #      retval, decoded_info, points, straight_qrcode = qcd.detectAndDecodeMulti(img)
+ #       urls.append(decoded_info)
     
-    return{ 'hits': num_results, 'imgs': imgs, 'urls' : urls  }
-#    return{'imgs': imgs, 'urls' : urls  }
+#    return{ 'hits': num_results, 'imgs': imgs, 'urls' : urls  }
+    
+    return{'imgs': imgs, 'urls' : urls  }
