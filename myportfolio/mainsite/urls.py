80edd90591e6cdf5536e4f546cebbea3bc317a38
stefano-lacorazza <112132737+stefano-lacorazza@users.noreply.github.com>
2023-08-20 21:59:36 -0500

    .

--- myportfolio/mainsite/urls.py
+++ myportfolio/mainsite/urls.py
@@ -3,7 +3,7 @@ from django.urls import include, path
 import project1
 import project2
 import project3
-import project4
+#import project4
 
 
 from . import views
@@ -13,6 +13,6 @@ urlpatterns = [
     path("project1/", include("project1.urls")),
     path("project2/", include("project2.urls")),
     path("project3/", include("project3.urls")),
-    path("QR-extractor/", include("project4.urls")),
+#  path("QR-extractor/", include("project4.urls")),
     path("admin/", admin.site.urls),
 ]
