0dc8958738cdf2d705803a6b2e606832dc04283a
stefano-lacorazza <112132737+stefano-lacorazza@users.noreply.github.com>
2023-08-21 02:33:53 -0500

    .

--- myportfolio/myportfolio/urls.py
+++ myportfolio/myportfolio/urls.py
@@ -18,12 +18,13 @@ from django.contrib import admin
 from django.urls import include, path
 from django.conf import settings  # new
 from django.conf.urls.static import static  # new
+from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("", include("mainsite.urls")),
     path('django_plotly_dash/', include('django_plotly_dash.urls')),
     
-]
-if settings.DEBUG:  
-        urlpatterns += static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT) 
+]+ static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT) 
+#if settings.DEBUG:  
+#        urlpatterns += static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT) 
