bf1ba48747ab71bab93802e1075ab3d677c47e90
stefano-lacorazza <112132737+stefano-lacorazza@users.noreply.github.com>
2023-08-20 22:59:27 -0500

    Update settings.py

--- myportfolio/myportfolio/settings.py
+++ myportfolio/myportfolio/settings.py
@@ -143,7 +143,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 STATIC_ROOT = "/home/laco89/portfolio/myportfolio/static"
 
 STATICFILES_DIRS = [
