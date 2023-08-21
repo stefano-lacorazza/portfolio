5f1f17ceb9ee255e9d5ba44f28d57e68be05a5cc
stefano-lacorazza <112132737+stefano-lacorazza@users.noreply.github.com>
2023-08-21 10:44:48 -0500

    Update predict.py

--- myportfolio/project4/predict.py
+++ myportfolio/project4/predict.py
@@ -20,10 +20,10 @@ def deleteQR():
         print("Error: %s : %s" % (dir_path, e.strerror))
 
 def predict(p_image):
-    model_path = os.path.join(os.path.dirname(__file__), 'best.pt')
+ #   model_path = os.path.join(os.path.dirname(__file__), 'best.pt')
 
-    with open(model_path, 'rb') as model_file:
-        model = pickle.load(model_file)
+ #   with open(model_path, 'rb') as model_file:
+ #       model = pickle.load(model_file)
 #    deleteQR()
  #   source = p_image
  #   results = model.predict(source, conf =0.7)
