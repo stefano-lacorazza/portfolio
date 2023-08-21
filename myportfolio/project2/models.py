018d192195d55b0f2b77a12e7370947880fbb7b6
stefano-lacorazza <112132737+stefano-lacorazza@users.noreply.github.com>
2023-08-21 16:51:09 -0500

    Update models.py

--- myportfolio/project2/models.py
+++ myportfolio/project2/models.py
@@ -1,9 +1,2 @@
 from django.db import models
 
-from location_field.models.plain import PlainLocationField
-
-
-class Place(models.Model):
-    city = models.CharField(max_length=255)
-    location = PlainLocationField(based_fields=['city'], zoom=7)
-
