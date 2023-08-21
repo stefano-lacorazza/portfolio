bebc49f98c1eea5e6f3fa83d07c88e52bcc4b84c
stefano-lacorazza <112132737+stefano-lacorazza@users.noreply.github.com>
2023-08-21 14:29:57 -0500

    Update views.py

--- myportfolio/project4/views.py
+++ myportfolio/project4/views.py
@@ -1,12 +1,13 @@
 from django.shortcuts import render
 from .forms import UploadFileForm
 #from django.http import HttpResponseRedirect
-from .predict import predict
+#from .predict import predict
 #from PIL import Image
 from django.core.files.uploadedfile import SimpleUploadedFile
 #import json 
 #from .models import QR
 # Create your views here.
+from roboflow import Roboflow
 def project4(request):
     form = UploadFileForm()
 
@@ -26,8 +27,11 @@ def upload_file(request):
             #obj.save()
             source = '\\media\\images\\'+str(img)
             #source = source.encode('unicode_escape')
-            dict = predict(source)
-            
+            #dict = predict(source)
+            rf = Roboflow(api_key="PNtIMFu4RUL4mGqZc01W")
+            project = rf.workspace().project("qr-code-detector-jx362")
+            model = project.version(1).model
+            model.predict(source, confidence=40, overlap=30).save("prediction.jpg")
             #context = {'hits': dict['hits'], 'imgs': json.dumps(dict['imgs']), 'urls': json.dumps(dict['urls'])}
             context = {'img':  source  }
             return render(request, "Project4/success4.html", context)
