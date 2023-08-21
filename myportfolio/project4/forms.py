e8955684f871c6a8a19e72422a39db7aa6f30108
stefano-lacorazza <112132737+stefano-lacorazza@users.noreply.github.com>
2023-08-21 02:53:54 -0500

    Update forms.py

--- myportfolio/project4/forms.py
+++ myportfolio/project4/forms.py
@@ -1,8 +1,8 @@
 from django import forms
 #from django.db import models  
-#from .models import UploadImage  
+from .models import UploadImage  
 
 class UploadFileForm(forms.ModelForm):
-    #models = UploadImage
+    models = UploadImage
     image = forms.ImageField(label="Image file")
     #fields = '__all__'  
