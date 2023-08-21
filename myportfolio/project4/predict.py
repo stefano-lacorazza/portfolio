95ddd6dbfe156862929bc9368f65bcdf9e76c538
stefano-lacorazza <112132737+stefano-lacorazza@users.noreply.github.com>
2023-08-20 22:22:16 -0500

    Update predict.py

--- myportfolio/project4/predict.py
+++ myportfolio/project4/predict.py
@@ -18,8 +18,8 @@ def predict(p_image):
     deleteQR()
     source = p_image
     results = model.predict(source, conf =0.7)
-#    num_results = len(results[0].boxes.data)
-#    qcd = cv2.QRCodeDetector()
+    num_results = len(results[0].boxes.data)
+    qcd = cv2.QRCodeDetector()
 #    for r in results:
 #            r.save_crop('')
 #    time.sleep(3)
