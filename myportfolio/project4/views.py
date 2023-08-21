ac8d787200e27fb6db64c72846cbddc73ec8c75b
stefano-lacorazza <112132737+stefano-lacorazza@users.noreply.github.com>
2023-08-21 00:27:51 -0500

    Update views.py

--- myportfolio/project4/views.py
+++ myportfolio/project4/views.py
@@ -24,4 +24,4 @@ def upload_file(request):
             #return HttpResponseRedirect("Project4/proyect4.html", context)
     else:
         form = UploadFileForm()
-    return render(request, "success4.html")
+    return render(request, "Project4/success4.html")
