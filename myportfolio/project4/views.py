c7b8292a2a01096a7991f08115e7376e0eb22319
stefano-lacorazza <112132737+stefano-lacorazza@users.noreply.github.com>
2023-08-21 00:49:06 -0500

    Update views.py

--- myportfolio/project4/views.py
+++ myportfolio/project4/views.py
@@ -18,10 +18,11 @@ def upload_file(request):
         #form = UploadFileForm(request.POST)
         if form.is_valid():
 
-            dict = predict(request.FILES["image"])
-            context = {'hits': dict['hits'], 'imgs': json.dumps(dict['imgs']), 'urls': json.dumps(dict['urls'])}
+            #dict = predict(request.FILES["image"])
+            #context = {'hits': dict['hits'], 'imgs': json.dumps(dict['imgs']), 'urls': json.dumps(dict['urls'])}
             #context = {'img': form.cleaned_data.get('text')}
-            return render(request, "Project4/success4.html", context)
+            #return render(request, "Project4/success4.html", context)
+            return render(request, "Project4/success4.html")
             #return HttpResponseRedirect("Project4/proyect4.html", context)
     else:
         form = UploadFileForm()
