47ccc5a7db84eb6ba9d131e6216aaeb001fc9921
stefano-lacorazza <112132737+stefano-lacorazza@users.noreply.github.com>
2023-08-21 02:34:54 -0500

    Update models.py

--- myportfolio/project4/models.py
+++ myportfolio/project4/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 
 class UploadImage(models.Model):
-    image = models.ImageField(upload_to = 'images')
+    image = models.ImageField(upload_to = 'images/')
 
     def __str__(self):
         return self.image
