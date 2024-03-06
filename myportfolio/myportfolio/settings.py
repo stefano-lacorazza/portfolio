ba96a57dcf050dd8f5c44b4180c72839737649a1
stefano-lacorazza <112132737+stefano-lacorazza@users.noreply.github.com>
2024-03-06 12:51:25 -0500

    Update settings.py

--- myportfolio/myportfolio/settings.py
+++ myportfolio/myportfolio/settings.py
@@ -152,12 +152,6 @@ USE_TZ = True
 
 STATIC_URL = "/static/"
 STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
-#STATIC_ROOT = r"C:/Users/laco-/OneDrive/Documentos/GitHub/portfolio/myportfolio/static"
-
-STATICFILES_DIRS = [
-    BASE_DIR / "static",
-    "/var/www/static/",
-]
 
 MEDIA_ROOT = r"C:/Users/laco-/OneDrive/Documentos/GitHub/portfolio/myportfolio/media"  
 MEDIA_URL = '/media/'
